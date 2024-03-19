4ac329aec50a69df8606e6fbc88d60631e819cd9
amirhamzh <amirhamzh3000@gmail.com>
2024-03-19 16:37:06 +0330

    create home app

--- A/home/views.py
+++ A/home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def sayhello(request):
+    return render(request,'hello.html')
+
