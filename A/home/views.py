7951810e1de5673db95df9e922af8e37141d40d3
amirhamzh <amirhamzh3000@gmail.com>
2024-03-21 17:37:32 +0330

    create todo form

--- A/home/views.py
+++ A/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect , render
 from .models import Todo
 from django.contrib import messages
+from .forms import TOdoCreateForm
 
 
 def home(request):
@@ -16,4 +17,16 @@ def delete(request,todo_id):
     Todo.objects.get(id =todo_id).delete()
     messages.success(request,"todo deleted successfully",'success')
     return  redirect('home')
+
+def create(request):
+    if request.method == 'POST':
+        form = TOdoCreateForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            Todo.objects.create(title = cd['title'],body = cd['body'],created = cd['created'])
+            messages.success(request,'todo created successfuly','success')
+            return redirect('home')
+    else:
+        form = TOdoCreateForm()
+    return render(request,'create.html',{'form':form})
 
